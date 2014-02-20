4c29e326e651d72f0e406dc0bbea295e25b09d69
jtran-ensae <julien.tran@ensae.fr>
2014-02-20 23:51:03 +0100

    projet terminé

--- figures.cpp
+++ figures.cpp
@@ -1,13 +1,9 @@
-/*
-On construit une classe "figures" pour afficher des nombres sur l'écran.
-L'affichage d'un chiffre est expliqué ci-dessous.
-*/
-
-
 #include "figures.h"
 
-
 /*
+On construit une classe "figures" pour afficher des nombres sur l'écran.
+L'affichage d'un chiffre est expliqu?ci-dessous.
+
 L'affichage d'un chiffre se fait par morceaux.
 Chaque morceau étant une des sept barres représentées ci-dessous.
 +---0---+
@@ -23,14 +19,14 @@ Chaque morceau étant une des sept barres représentées ci-dessous.
 
 
 /*
-Dans le code,un chiffre est représenté par une table de 7 bool.
+Dans le code,un chiffre est représent?par une table de 7 bool.
 Chaque bool représente une barre parmi les 7 représentées ci-dessus.
 si bool = 1 alors la barre correspondante est affichée.
 si bool = 0 alors la barre correspondante n'est pas affichée.
 */
 
 
-//On construit ci-dessous les dix chiffres de 0 à 9.
+//On construit ci-dessous les dix chiffres de 0 ?9.
 bool bar[10][7] = { 
 { 1, 1, 1, 1, 1, 1, 0 },
 { 0, 1, 1, 0, 0, 0, 0 },
@@ -51,7 +47,6 @@ figures::figures(long int number, int x, int y, Uint32 color, bool rate)
 		fprintf(stderr, "nombre trop grand !!\n");
 	}
 
-	//Voir parameters.h pour les commentaires sur les champs.
 	_screen = new Screen();
 	_number = number;
 	_x = x;
@@ -59,7 +54,6 @@ figures::figures(long int number, int x, int y, Uint32 color, bool rate)
 	_color = color;
 	_rate = rate;
 
-	//On definit les 7 barres pour construire l'affichage d'un chiffre (voir la fonction defRect).
 	defRect(R[0], _x, _y, H, L);
 	defRect(R[1], _x + H - L, _y, L, H + L);
 	defRect(R[2], _x + H - L, _y + H + L, L, H);
@@ -69,7 +63,6 @@ figures::figures(long int number, int x, int y, Uint32 color, bool rate)
 	defRect(R[6], _x, _y + L + H, H, L);
 	defRect(point, _x , _y + 2 * H + L, L, L);
 
-	//recalculate() récupère tous les chiffres qui composent le nombre: number.
 	recalculate();
 
 }
@@ -78,63 +71,63 @@ figures::figures(long int number, int x, int y, Uint32 color, bool rate)
 //refreshInt permet d'afficher un entier.
 void figures::refreshInt()
 {
-	//recalculate() récupère tous les chiffres qui composent le nombre: number.
+	//recalculate() récupère tous les chiffres qui composent le nombre: _number.
 	recalculate();
 
-	//On retire tous les zéros à gauche. 
+	//On retire tous les zéros ?gauche. 
 	int i = 5, shift = 0;
 	while (digit[i] == 0 && i>0)i--;
 
-	//On affiche les chiffres sur la surface screen du chiffre de plus grande puissance de 10 (digit[i]) vers le chiffre des unités (digit[0]).
+	//On affiche le chiffre de plus grande puissance de 10 (digit[i]) vers le chiffre des unités (digit[0]).
 	for (i; i >= 0; i--)
 	{
 		//On s'occupe de la barre j du chiffre digit[i], cette barre étant représentée par la surface R[j].
 		//On rappelle que les éléments bar[k][l] sont des bool.
 		//Si bar[k][j]= TRUE (ie =1) alors on remplit le surface rectangulaire R[j] par la couleur: _color.
-		for (int j = 0; j <= 6; j++) if (bar[digit[i]][j]) SDL_FillRect(_screen->GetScreen(), &R[j], _color);
+		for (int j = 0; j <= 6; j++) if (bar[digit[i]][j]) SDL_FillRect(_screen->getScreen(), &R[j], _color);
 
 		//On affiche les chiffres (ie les surfaces R[j]), du chiffre des unités vers le chiffre de plus grande puissance de 10.
 		//On note que la largeur d'un chiffre fait H+2L.
 		//On choisit de laisser un espace de 2L entre chaque chiffre.
-		//On décale donc l'affichage de H + 4 * L à chaque fois qu'on change de chiffre à afficher.
-		//Ceci correspond simplement à ajouter H + 4 * L à l'abscisse de chaque barre.
+		//On décale donc l'affichage de H + 4 * L ?chaque fois qu'on change de chiffre ?afficher.
+		//Ceci correspond simplement ?ajouter H + 4 * L ?l'abscisse de chaque barre.
 		for (int j = 0; j <= 6; j++) R[j].x += H + 4 * L;
 
 		//Shift compte le nombre de chiffres.
 		shift++;
 	}
 
-	//Comme les barres R[j] on été décalées, il faut réinitialiser leurs positions.
-	for (int l = 0; l < shift; l++)						//Les barres ont été décalées shift fois.
-	for (int k = 0; k <= 6; k++) R[k].x -= H + 4 * L;	//A chaque fois, chaque barre a été décalée de H + 4 * L.
+	//Comme les barres R[j] on ét?décalées, il faut réinitialiser leurs positions.
+	for (int l = 0; l < shift; l++)						//Les barres ont ét?décalées shift fois.
+	for (int k = 0; k <= 6; k++) R[k].x -= H + 4 * L;	//A chaque fois, chaque barre a ét?décalée de H + 4 * L.
 
 }
 
 
-//refresh() permet d'afficher les entiers et aussi les taux (de probabilité 0.25 ...).
+//refresh() permet d'afficher les entiers et aussi les taux (de probabilit? 0.25 ...).
 void figures::refresh()
 {
-	//Si c'est un taux de probabilité alors:
+	//Si c'est un taux de probabilit?alors:
 	if (_rate == 1){	
-		//Si c'est strictement inferieur à 10% (ie 0.10) alors on ajoute un ".0" avant d'écrire le nombre (_number = 8 par exemple). Et on obtient bien .08.
+		//Si c'est strictement inferieur ?10% (ie 0.10) alors on ajoute un ".0" avant d'écrire le nombre (_number = 8 par exemple). Et on obtient bien .08.
 		if (_number < 10){
 
 			//On remplit la surface de la virgule avec la couleur _color.
-			SDL_FillRect(_screen->GetScreen(), &point, _color);		
+			SDL_FillRect(_screen->getScreen(), &point, _color);		
 
-			//On décale toutes les barres des chiffres de 3L vers la droite car la virgule est un carré de coté L, et on veut un espace de 2L après la virgule: L + 2L = 3L.
+			//On décale toutes les barres des chiffres de 3L vers la droite car la virgule est un carr?de cot?L, et on veut un espace de 2L après la virgule: L + 2L = 3L.
 			for (int j = 0; j <= 6; j++) R[j].x += 3 * L;
 
-			//Comme on a un taux < 10%, le premier chiffre après la virgule est un 0: on affiche un 0. On a alors ".0".
-			for (int j = 0; j <= 6; j++) if (bar[0][j]) { SDL_FillRect(_screen->GetScreen(), &R[j], _color);}
+			//Comme on a un taux < 10%, le premier chiffre après la virgule est un 0: on affiche un 0 et on a ".0".
+			for (int j = 0; j <= 6; j++) if (bar[0][j]) { SDL_FillRect(_screen->getScreen(), &R[j], _color);}
 
 			//On décale toutes les barres de H + 4L car on vient d'ajouter un 0 de largeur H+2L et on veut un espace de 2L avec le prochain chiffre: H+2L + 2L = H+4L.
 			for (int j = 0; j <= 6; j++) R[j].x += H + 4 * L;
 
-			//Ayant déjà la virgule et le premier chiffre 0, il ne manque plus qu'à ajouter _number. On a alors ".08".
+			//Ayant déj?la virgule et le premier chiffre 0, il ne manque plus qu'?ajouter _number. On a alors ".08".
 			refreshInt();
 
-			//On réinitialise l'abscisse de chacune des 7 barres car avait décalé ces barres à deux reprises.
+			//On réinitialise l'abscisse de chacune des 7 barres car on avait décal?ces barres ?deux reprises.
 			for (int j = 0; j <= 6; j++) R[j].x -= H + 4 * L;
 			for (int j = 0; j <= 6; j++) R[j].x -= 3 * L;
 		}
@@ -142,12 +135,12 @@ void figures::refresh()
 		else{
 
 			//On remplit la surface de la virgule avec la couleur _color.
-			SDL_FillRect(_screen->GetScreen(), &point, _color);
+			SDL_FillRect(_screen->getScreen(), &point, _color);
 
-			//On décale toutes les barres des chiffres de 3L vers la droite car la virgule est un carré de coté L, et on veut un espace de 2L après la virgule: L + 2L = 3L.
+			//On décale toutes les barres des chiffres de 3L vers la droite car la virgule est un carr?de cot?L, et on veut un espace de 2L après la virgule: L + 2L = 3L.
 			for (int j = 0; j <= 6; j++) R[j].x += 3 * L;
 
-			//Ayant déjà la virgule, il ne manque plus qu'à ajouter _number. On a alors ".77".
+			//Ayant déj?la virgule, il ne manque plus qu'?ajouter _number. On a alors ".77".
 			refreshInt();
 			for (int j = 0; j <= 6; j++) R[j].x -= 3 * L;
 		}
@@ -156,33 +149,31 @@ void figures::refresh()
 	else{
 		refreshInt();
 	}
-	SDL_Flip(_screen->GetScreen());
+	SDL_Flip(_screen->getScreen());
 }
 
 
 
-//La fonction remove() efface l'affichage d'un nombre.
+//La fonction remove() efface l'affichage d'un nombre. 
 void figures::remove()
 {
 	Uint32 C = _color;
 	_color = 0;			//Pour effacer l'affichage d'un nombre il suffit de réécrire ce nombre avec la couleur du fond.
 	refresh();
-	_color = C;			//On pense à remettre la couleur choisie pour afficher les prochains nombres. 
+	_color = C;			//On pense ?remettre la couleur ?l'état initial. 
 }
 
 
-//Decrement permet de décrementer un nombre affiché (Un nombre affiché étant un figures et non un int).
 void figures::decrement()
 {
-	(_number)--; //Il suffit de décrementer le nombre number.
-	refresh();	//Puis de l'afficher.
+	(_number)--; 
+	refresh();	
 }
 
-//Increment permet d'incrémenter un nombre affiché (Un nombre affiché étant un figures et non un int).
 void figures::increment()
 {
-	(_number)++;	//Il suffit d'incrementer le nombre number.
-	refresh();		//Puis de l'afficher.
+	(_number)++;
+	refresh();
 }
 
 
@@ -191,9 +182,9 @@ void figures::increment()
 //Si on considère _number=edcba, la fonction recalculate récupère: digit[0] = a, digit[1] = b ...
 void figures::recalculate()
 {
-	int coeff = 0, temp = _number;		//On rappelle que _number est le nombre à afficher.
+	int coeff = 0, temp = _number;		//On rappelle que _number est le nombre ?afficher.
 
-	for (int i = 5; i >= 0; i--)		//On se limite au maximum à 99 999 donc on a au maximum 5 chiffres.
+	for (int i = 5; i >= 0; i--)		//On se limite au maximum ?99 999 donc on a au maximum 5 chiffres.
 	{
 		digit[i] = 0;
 		if (temp >= power10(i)) {
@@ -208,8 +199,8 @@ void figures::recalculate()
 //defRect() construit une barre (d'un chiffre) par une surface rectangulaire R.
 void figures::defRect(SDL_Rect& R, int x, int y, int w, int h)
 {
-	R.x = x;	//R.x est l'abscisse du point le plus haut à gauche de la surface R.
-	R.y = y;	//R.y est l'abscisse du point le plus haut à gauche de la surface R.
+	R.x = x;	//R.x est l'abscisse du point le plus haut ?gauche de la surface R.
+	R.y = y;	//R.y est l'abscisse du point le plus haut ?gauche de la surface R.
 	R.w = w;	//R.w est la largeur de cette surface.
 	R.h = h;	//R.h est la hauteur de cette surface.
 }
@@ -228,12 +219,22 @@ long int figures::getNumber()
 	return _number;
 }
 
-void figures::setNumber(int x){
+void figures::setNumber(int x)
+{
 	_number = x;
+}
+
+//Update() met ?jour le nombre affich?
+void figures::update(int x)
+{
+	remove();
+	_number = x;
+	refresh();
 }
 
 figures::~figures()
 {
+	delete _screen;
 }
 
 
--- figures.h
+++ figures.h
@@ -1,22 +1,17 @@
+#pragma once
+#include "Screen.h"
+#include <stdlib.h> //pour exit
+#include <stdio.h>
+
 /*
 On construit une classe "figures" pour afficher des nombres sur l'écran.
-Le nombre à afficher est soit un entier, soit un taux (de probabilité) entre  0 et 1 avec deux chiffres après la virgule.
-Que ce soit un entier ou un taux, le champs correspondant (_number) est un long int.
+Le nombre ?afficher est soit un entier, soit un taux (de probabilit? entre  0 et 1 avec deux chiffres après la virgule.
+Que ce soit un entier ou un taux, le champs correspondant ?_number est un long int.
 C'est le champs bool _rate qui distingue les entiers (_rate=0) des taux (_rate=1).
-On explique au début du figures.cpp comment est représenté un chiffre.
+On explique au début du figures.cpp comment est représent?un chiffre.
 */
 
-
-#ifndef FIGURES_H
-#include <stdlib.h> //pour exit et atexit
-#include <stdio.h>
-#include <sdl.h>
-#define FIGURES_H
-#endif
-
-#include "Screen.h"
-
-//Comme expliqué dans le figures.cpp, un chiffre est construit par un assemblage de barres.
+//Comme expliqu?dans figures.cpp, un chiffre est construit par un assemblage de barres.
 //Une barre étant une surface rectangulaire de dimensions H x L.
 const int H = 14;
 const int L = 2;
@@ -24,18 +19,16 @@ const int L = 2;
 class figures
 {
 protected:
-	long int _number;										//Le nombre à afficher. 
+	long int _number;										//Le nombre ?afficher. 
 	int digit[6];											//Le chiffre de la puissance 10^i.
 	Screen* _screen;
-	//SDL_Surface* _screen;									//La surface où doit être affiché le nombre.
-	int _x, _y;												//Ce sont les coordonnées du point du coin en haut à gauche du nombre à afficher.			
+	int _x, _y;												//Ce sont les coordonnées du point du coin en haut ?gauche du nombre ?afficher.			
 	SDL_Rect R[7];											//Il nous faut sept surfaces rectangulaires pour les 7 barres qui forment un chiffres.
-	SDL_Rect point;											//Surface pour afficher une virgule (pour les nombres décimaux).
+	SDL_Rect point;											//Surface pour afficher une virgule.
 	Uint32 _color;											//La couleur utilisée pour les chiffres.
-	bool _rate;												//Indique si le nombre est un entier ou un taux (de probabilité).
-															//Un taux est un décimal entre 0 et 1 et doit donc être précédé d'une virgule.
+	bool _rate;												//Indique si le nombre est un entier ou un taux (de probabilit?.
+															//Un taux est un décimal entre 0 et 1 et doit donc être précéd?d'une virgule.
 
-	//Voir figures.cpp pour les commentaires.
 	void recalculate();														
 	long int power10(int exponent);							
 	void defRect(SDL_Rect& R, int x, int y, int w, int h);	
@@ -43,14 +36,14 @@ protected:
 
 public:
 	figures(long int number, int x, int y, Uint32 color, bool rate);
-	~figures();
-	//Voir figures.cpp pour les commentaires.										
+	~figures();										
 	void refresh();											
 	void remove();											
 	void decrement();									
 	void increment();	
 	long int getNumber();
 	void setNumber(int x);
+	void update(int x);
 };
 
 
